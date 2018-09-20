aa8095a8654712c985240179804254bf77b30246
Rafal Migda <rafal@netpistols.com>
2018-09-20 14:46:03 +0200

    Upgrade laravel version to 5.7, upgrade phpunit version to 6

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,16 +7,6 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
@@ -24,5 +14,15 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    /**
+     * Show welcome message
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function welcome()
+    {
+        return view('welcome');
     }
 }
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# Laravel 5.4 boilerplate with Vue.js admin area + REST API
+# Laravel 5.7 boilerplate with Vue.js 2 admin area + REST API
 
 Role based (user / admin) Laravel boilerplate application with admin area written in Vue.js + Vue Router + REST API.
 
@@ -6,8 +6,8 @@ Admin area (after successful login + auth as an admin): http://127.0.0.1:8000/ad
 
 ##Libraries and frameworks I used in this project:
 
-- Laravel 5.4
-- Vue.js 2.4.2
+- Laravel 5.7
+- Vue.js 2.5.17
     - VueRouter
     - VueResource
     - VueSweetalert
